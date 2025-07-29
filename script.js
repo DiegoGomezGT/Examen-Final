628d79a49226bd1aeff038aaabc1198ea6e93063
DiegoGomezGT <al45663@un.edu.mx>
2025-07-29 10:09:41 -0600

    Avance

---
+++ script.js
@@ -0,0 +1,6 @@
+ document.getElementById('nombre').addEventListener('click', function() {
+      this.textContent = "Diega Gómez";
+    });
+document.getElementById('ocupacion').addEventListener('click', function() {
+      this.innerHTML = "<strong>Ocupación:</strong> Alumna";
+    });
